d1d07200f0f32fe0bc5e90e4771ffaa23c624a3b
VanshMaradiya <vanshmaradiya009@gmail.com>
2026-03-01 23:45:36 +0530

    Clean up
    Removed commented lines for clarity and tidiness.

--- src/gui_app.py
+++ src/gui_app.py
@@ -11,14 +11,13 @@ def run_script(script_name: str):
     Example: run_script("register_face.py")
     """
     try:
-        #  Always run inside src folder
+       
         script_path = os.path.join(os.path.dirname(__file__), script_name)
 
         if not os.path.exists(script_path):
             messagebox.showerror("Error", f"File not found:\n{script_path}")
             return
 
-        # Run script with same python interpreter
         subprocess.Popen([sys.executable, script_path])
 
     except Exception as e:
@@ -33,7 +32,6 @@ class SmartAttendanceGUI:
         self.root.geometry("500x500")
         self.root.resizable(False, False)
 
-        #  Title
         title = tk.Label(root,
                          text="Smart Attendance System",
                          font=("Arial", 20, "bold"))
@@ -41,38 +39,32 @@ class SmartAttendanceGUI:
 
         subtitle = tk.Label(root, text="Admin Dashboard", font=("Arial", 12))
         subtitle.pack(pady=5)
-
-        #  Buttons Frame
         frame = tk.Frame(root)
         frame.pack(pady=20)
 
         btn_style = {"font": ("Arial", 12), "width": 25, "height": 2}
 
-        #  Register Face
+
         tk.Button(frame,
                   text="1) Register Face",
                   command=lambda: run_script("register_face.py"),
                   **btn_style).pack(pady=8)
 
-        #  Train Model
         tk.Button(frame,
                   text="2) Train Model",
                   command=lambda: run_script("train_model.py"),
                   **btn_style).pack(pady=8)
 
-        #  Mark Attendance
         tk.Button(frame,
                   text="3) Mark Attendance",
                   command=lambda: run_script("mark_attendance.py"),
                   **btn_style).pack(pady=8)
 
-        #  Attendance Report GUI
         tk.Button(frame,
                   text="4) Attendance Report",
                   command=lambda: run_script("attendance_report_gui.py"),
                   **btn_style).pack(pady=8)
 
-        #  Exit
         tk.Button(frame,
                   text="Exit",
                   command=self.root.quit,
